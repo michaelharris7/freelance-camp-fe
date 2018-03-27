d66b9cab547b1de9ed60f188b7c29201f7b08146
Michael Harris <michaeljh7@gmail.com>
2018-03-27 15:04:46 -0600

    Fixed error on proposal.ts

--- src/app/proposal/proposal.ts
+++ src/app/proposal/proposal.ts
@@ -2,9 +2,9 @@ export class Proposal {
   constructor(
     public id?: number,
     public customer?: string,
-    public portfolio_url?: string = 'http://',
+    public portfolio_url: string = 'http://',
     public tools?: string,
-    public estimated_hours?: string,
+    public estimated_hours?: number,
     public hourly_rate?: number,
     public weeks_to_complete?: number,
     public client_email?: string,
