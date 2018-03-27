d1dc6838476cbe011cd45332bab704a2368b578c
Michael Harris <michaeljh7@gmail.com>
2018-03-27 11:27:21 -0600

    Added proposal

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://www.harriscoding.com', 'Ruby on Rails', 150, 120, 15, 'mike@harriscoding.com')
+  proposalTwo: Proposal = new Proposal(90, 'XYZ Company', 'http://www.harriscoding.com', 'Ruby on Rails', 150, 120, 15, 'mike@harriscoding.com')
+  proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://www.harriscoding.com', 'Ruby on Rails', 150, 120, 15, 'mike@harriscoding.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
