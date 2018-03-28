e0d23136dd35ea98db269e44f420c0102215fcff
Michael Harris <michaeljh7@gmail.com>
2018-03-28 15:03:00 -0600

    Implemented bootstrap card styles and added document examples

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
       description: 'asdfasdfasdf asdfasdf',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://googlecom',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     },
     {
       title: "My Second Doc",
       description: 'asdfasdfasdf asdfasdf',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://googlecom',
+      image_url: 'https://c.pxhere.com/photos/de/59/office_freelancer_computer_business_work_laptop_creative_iphone-909729.jpg!d'
     },
     {
       title: "My Last Doc",
       description: 'asdfasdfasdf asdfasdf',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://googlecom',
+      image_url: 'https://c.pxhere.com/photos/2d/2e/office_freelancer_computer_business_work_laptop_creative_iphone-537951.jpg!d'
     }
   ]
 }
