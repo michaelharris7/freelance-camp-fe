8fef67266d87106a40af1a55f1458a485b87ff95
Michael Harris <michaeljh7@gmail.com>
2018-03-30 12:21:01 -0600

    Made direct connection with microservice and implemented rendering of API data

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
 
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
-    },
-    {
-      title: "My Second Doc",
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://c.pxhere.com/photos/de/59/office_freelancer_computer_business_work_laptop_creative_iphone-909729.jpg!d'
-    },
-    {
-      title: "My Last Doc",
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://c.pxhere.com/photos/2d/2e/office_freelancer_computer_business_work_laptop_creative_iphone-537951.jpg!d'
-    }
-  ]
+  constructor(
+    private documentService: DocumentService;
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
